b9a5af974c51e5f2efd50abde8968aee40de0a92
ZYW-Mia <63743190+ZYW-Mia@users.noreply.github.com>
2022-05-17 13:33:18 +0200

    Add files via upload

---
+++ positions_comparison/js/data.js
@@ -0,0 +1,667 @@
+let data_2022 = [{
+        name: 'Harvard',
+        value: 112
+    },
+    {
+        name: 'UC Berkeley',
+        value: 112
+    },
+    {
+        name: 'Chicago',
+        value: 139
+    },
+    {
+        name: 'Columbia',
+        value: 44
+    },
+    {
+        name: 'Michigan',
+        value: 108
+    },
+    {
+        name: 'Yale',
+        value: 55
+    },
+    {
+        name: 'UCLA',
+        value: 47
+    },
+    {
+        name: 'Cornell',
+        value: 24
+    },
+    {
+        name: 'Pennsylvania',
+        value: 47
+    },
+    {
+        name: 'Arizona',
+        value: 64
+    },
+    {
+        name: 'Wisconsin Madison',
+        value: 23
+    },
+    {
+        name: 'Oregon',
+        value: 13
+    },
+    {
+        name: 'Stanford',
+        value: 55
+    },
+    {
+        name: 'Northwestern',
+        value: 24
+    },
+    {
+        name: 'Washington',
+        value: 33
+    },
+    {
+        name: 'Colorado',
+        value: 12
+    },
+    {
+        name: 'Minnesota',
+        value: 13
+    },
+    {
+        name: 'Pittsburgh',
+        value: 12
+    },
+    {
+        name: 'Tulane',
+        value: 12
+    },
+    {
+        name: 'New Mexico',
+        value: 37
+    },
+    {
+        name: 'Michigan State',
+        value: 7
+    },
+    {
+        name: 'Southern Illinois',
+        value: 1
+    },
+    {
+        name: 'Kansas',
+        value: 4
+    },
+    {
+        name: 'Syracuse',
+        value: 7
+    },
+    {
+        name: 'Texas State',
+        value: 1
+    },
+    {
+        name: 'Hawaii',
+        value: 9
+    },
+    {
+        name: 'UC Davis',
+        value: 29
+    },
+    {
+        name: 'Brandeis',
+        value: 5
+    },
+    {
+        name: 'SUNY Buffalo',
+        value: 5
+    },
+    {
+        name: 'Pennsylvania State',
+        value: 29
+    },
+    {
+        name: 'Ohio State',
+        value: 8
+    },
+    {
+        name: 'Missouri',
+        value: 9
+    },
+    {
+        name: 'Washington State',
+        value: 8
+    },
+    {
+        name: 'Rochester',
+        value: 4
+    },
+    {
+        name: 'Duke',
+        value: 24
+    },
+    {
+        name: 'Utah',
+        value: 5
+    },
+    {
+        name: 'UC Santa Barbara',
+        value: 30
+    },
+    {
+        name: 'Princeton',
+        value: 20
+    },
+    {
+        name: 'Massachusetts',
+        value: 18
+    },
+    {
+        name: 'NYU',
+        value: 49
+    },
+    {
+        name: 'New School',
+        value: 5
+    },
+    {
+        name: 'Brown',
+        value: 14
+    },
+    {
+        name: 'Boston University',
+        value: 6
+    },
+    {
+        name: 'Case Western Reserve',
+        value: 2
+    },
+    {
+        name: 'UC Riverside',
+        value: 1
+    },
+    {
+        name: 'CUNY',
+        value: 42
+    },
+    {
+        name: 'Florida',
+        value: 35
+    },
+    {
+        name: 'Georgia',
+        value: 14
+    },
+    {
+        name: 'Southern Methodist',
+        value: 9
+    },
+    {
+        name: 'Kentucky',
+        value: 8
+    },
+    {
+        name: 'Iowa',
+        value: 4
+    },
+    {
+        name: 'Arizona State',
+        value: 25
+    },
+    {
+        name: 'Rice',
+        value: 7
+    },
+    {
+        name: 'UC Irvine',
+        value: 5
+    },
+    {
+        name: 'Wayne State',
+        value: 1
+    },
+    {
+        name: 'Rutgers',
+        value: 15
+    },
+    {
+        name: 'Virginia',
+        value: 8
+    },
+    {
+        name: 'Oklahoma',
+        value: 5
+    },
+    {
+        name: 'Purdue',
+        value: 1
+    },
+    {
+        name: 'American',
+        value: 3
+    },
+    {
+        name: 'Connecticut',
+        value: 3
+    },
+    {
+        name: 'SUNY Stony Brook',
+        value: 23
+    },
+    {
+        name: 'UC San Diego',
+        value: 19
+    },
+    {
+        name: 'SUNY Binghamton',
+        value: 11
+    },
+    {
+        name: 'SUNY Albany',
+        value: 9
+    },
+    {
+        name: 'Wisconsin Milwaukee',
+        value: 6
+    },
+    {
+        name: 'Temple',
+        value: 5
+    },
+    {
+        name: 'UT Austin',
+        value: 43
+    },
+    {
+        name: 'UIUC',
+        value: 30
+    },
+    {
+        name: 'WUSTL',
+        value: 25
+    },
+    {
+        name: 'UNC',
+        value: 24
+    },
+    {
+        name: 'IUB',
+        value: 23
+    },
+    {
+        name: 'Emory',
+        value: 21
+    },
+    {
+        name: 'UC Santa Cruz',
+        value: 16
+    },
+    {
+        name: 'JHU',
+        value: 15
+    },
+    {
+        name: 'TAMU',
+        value: 11
+    },
+    {
+        name: 'Tennessee',
+        value: 11
+    },
+    {
+        name: 'MIT',
+        value: 10
+    },
+    {
+        name: 'Vanderbilt',
+        value: 10
+    },
+    {
+        name: 'South Florida',
+        value: 6
+    },
+    {
+        name: 'GWU',
+        value: 5
+    },
+    {
+        name: 'Iowa State',
+        value: 5
+    },
+    {
+        name: 'Alaska',
+        value: 4
+    },
+    {
+        name: 'Louisiana State',
+        value: 4
+    },
+    {
+        name: 'UC San Francisco',
+        value: 4
+    },
+    {
+        name: 'Nevada Reno',
+        value: 3
+    },
+    {
+        name: 'USC',
+        value: 3
+    },
+    {
+        name: 'Wyoming',
+        value: 3
+    },
+    {
+        name: 'Maryland',
+        value: 2
+    },
+    {
+        name: 'Nevada Las Vegas',
+        value: 2
+    },
+    {
+        name: 'South Carolina',
+        value: 2
+    },
+    {
+        name: 'UIC',
+        value: 2
+    },
+    {
+        name: 'William & Mary',
+        value: 2
+    },
+    {
+        name: 'Alabama',
+        value: 1
+    },
+    {
+        name: 'Colorado State',
+        value: 1
+    },
+    {
+        name: 'Maine',
+        value: 1
+    },
+    {
+        name: 'Montana',
+        value: 1
+    },
+    {
+        name: 'Oregon State',
+        value: 1
+    },
+    {
+        name: 'Southern California',
+        value: 1
+    }
+];
+
+let data_1976 = [{
+        name: 'Harvard',
+        value: 234
+    },
+    {
+        name: 'UC Berkeley',
+        value: 214
+    },
+    {
+        name: 'Chicago',
+        value: 200
+    },
+    {
+        name: 'Columbia',
+        value: 197
+    },
+    {
+        name: 'Michigan',
+        value: 118
+    },
+    {
+        name: 'Yale',
+        value: 95
+    },
+    {
+        name: 'UCLA',
+        value: 91
+    },
+    {
+        name: 'Cornell',
+        value: 90
+    },
+    {
+        name: 'Pennsylvania',
+        value: 86
+    },
+    {
+        name: 'Arizona',
+        value: 82
+    },
+    {
+        name: 'Wisconsin Madison',
+        value: 69
+    },
+    {
+        name: 'Oregon',
+        value: 60
+    },
+    {
+        name: 'Stanford',
+        value: 53
+    },
+    {
+        name: 'Northwestern',
+        value: 49
+    },
+    {
+        name: 'Washington',
+        value: 44
+    },
+    {
+        name: 'Colorado',
+        value: 40
+    },
+    {
+        name: 'Minnesota',
+        value: 33
+    },
+    {
+        name: 'Pittsburgh',
+        value: 28
+    },
+    {
+        name: 'Tulane',
+        value: 28
+    },
+    {
+        name: 'New Mexico',
+        value: 21
+    },
+    {
+        name: 'Michigan State',
+        value: 20
+    },
+    {
+        name: 'Southern Illinois',
+        value: 19
+    },
+    {
+        name: 'Kansas',
+        value: 18
+    },
+    {
+        name: 'Syracuse',
+        value: 17
+    },
+    {
+        name: 'Texas State',
+        value: 16
+    },
+    {
+        name: 'Hawaii',
+        value: 15
+    },
+    {
+        name: 'UC Davis',
+        value: 14
+    },
+    {
+        name: 'Brandeis',
+        value: 13
+    },
+    {
+        name: 'SUNY Buffalo',
+        value: 13
+    },
+    {
+        name: 'Pennsylvania State',
+        value: 11
+    },
+    {
+        name: 'Ohio State',
+        value: 11
+    },
+    {
+        name: 'Missouri',
+        value: 9
+    },
+    {
+        name: 'Washington State',
+        value: 9
+    },
+    {
+        name: 'Rochester',
+        value: 9
+    },
+    {
+        name: 'Duke',
+        value: 8
+    },
+    {
+        name: 'Utah',
+        value: 8
+    },
+    {
+        name: 'UC Santa Barbara',
+        value: 7
+    },
+    {
+        name: 'Princeton',
+        value: 7
+    },
+    {
+        name: 'Massachusetts',
+        value: 7
+    },
+    {
+        name: 'NYU',
+        value: 6
+    },
+    {
+        name: 'New School',
+        value: 6
+    },
+    {
+        name: 'Brown',
+        value: 5
+    },
+    {
+        name: 'Boston University',
+        value: 5
+    },
+    {
+        name: 'Case Western Reserve',
+        value: 5
+    },
+    {
+        name: 'UC Riverside',
+        value: 5
+    },
+    {
+        name: 'CUNY',
+        value: 4
+    },
+    {
+        name: 'Florida',
+        value: 4
+    },
+    {
+        name: 'Georgia',
+        value: 4
+    },
+    {
+        name: 'Southern Methodist',
+        value: 4
+    },
+    {
+        name: 'Kentucky',
+        value: 4
+    },
+    {
+        name: 'Iowa',
+        value: 4
+    },
+    {
+        name: 'Arizona State',
+        value: 3
+    },
+    {
+        name: 'Rice',
+        value: 3
+    },
+    {
+        name: 'UC Irvine',
+        value: 3
+    },
+    {
+        name: 'Wayne State',
+        value: 3
+    },
+    {
+        name: 'Rutgers',
+        value: 2
+    },
+    {
+        name: 'Virginia',
+        value: 2
+    },
+    {
+        name: 'Oklahoma',
+        value: 2
+    },
+    {
+        name: 'Purdue',
+        value: 2
+    },
+    {
+        name: 'American',
+        value: 1
+    },
+    {
+        name: 'Connecticut',
+        value: 1
+    },
+    {
+        name: 'SUNY Stony Brook',
+        value: 0
+    },
+    {
+        name: 'UC San Diego',
+        value: 0
+    },
+    {
+        name: 'SUNY Binghamton',
+        value: 0
+    },
+    {
+        name: 'SUNY Albany',
+        value: 0
+    },
+    {
+        name: 'Wisconsin Milwaukee',
+        value: 0
+    },
+    {
+        name: 'Temple',
+        value: 0
+    }
+]
